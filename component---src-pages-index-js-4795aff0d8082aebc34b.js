27443b325351b037800bf4c6700b6719ad29991a
Dragoș Străinu <str.dr4605@gmail.com>
2020-02-01 17:51:41 +0200

    Updates

---
+++ component---src-pages-index-js-4795aff0d8082aebc34b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Wbzz"),c=n("Bl7J");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement("h1",null,"Hi people"),l.a.createElement("p",null,"Welcome to your new Gatsby + Tailwind CSS + Styled Components site"),l.a.createElement("p",null,"Now go build something great."),l.a.createElement(o.Link,{to:"/page-2/"},"Go to page 2"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-4795aff0d8082aebc34b.js.map
