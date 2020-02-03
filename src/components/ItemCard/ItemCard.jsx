5c31df471b7a9b8e15f7014a9b61a26f32893c76
Dragoș Străinu <str.dr4605@gmail.com>
2020-02-03 23:18:32 +0200

    Change JSON, get data from JSON

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import * as s from './ItemCard.styled'
 
-
-
-export const ItemCard = () => (
+export const ItemCard = ({ item }) => (
   <s.ItemContainer>
-    <s.ItemImg src="https://via.placeholder.com/300x400"/>
-    <s.ItemInfo>Some info</s.ItemInfo>
+    <s.ItemImg src={item.imgSrc} />
+    <s.ItemInfo>{item.info}</s.ItemInfo>
   </s.ItemContainer>
 )
